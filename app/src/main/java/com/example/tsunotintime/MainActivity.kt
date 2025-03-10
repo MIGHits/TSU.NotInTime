133f6979a4494b65d85f1786f2e03edecd2849f8
MIGHits <164134306+MIGHits@users.noreply.github.com>
2025-03-10 11:26:51 +0700

    feat profile form UI

--- app/src/main/java/com/example/tsunotintime/MainActivity.kt
+++ app/src/main/java/com/example/tsunotintime/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -22,8 +24,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -32,13 +37,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
@@ -51,10 +61,14 @@ import com.example.tsunotintime.presentation.state.RegisterEvent
 import com.example.tsunotintime.presentation.viewModel.LoginViewModel
 import com.example.tsunotintime.presentation.viewModel.RegisterViewModel
 import com.example.tsunotintime.ui.theme.Nunito
+import com.example.tsunotintime.ui.theme.PrimaryButton
 import com.example.tsunotintime.ui.theme.PrimaryColor
 import com.example.tsunotintime.ui.theme.SecondaryButton
 import com.example.tsunotintime.ui.theme.SecondaryColor
 import com.example.tsunotintime.ui.theme.TSUNotInTimeTheme
+import com.example.tsunotintime.ui.theme.editButtonBackground
+import com.example.tsunotintime.ui.theme.exitButtonBackground
+import com.example.tsunotintime.ui.theme.exitButtonIconTint
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -66,16 +80,17 @@ class MainActivity : ComponentActivity() {
             TSUNotInTimeTheme {
                 val navController = rememberNavController()
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                         .consumeWindowInsets(PaddingValues())
                         .imePadding()
                 ) { innerPadding ->
                     ProfileScreen()
-                   /* NavigationScreen(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .background(color = SecondaryColor), navController = navController
-                    )*/
+                    /* NavigationScreen(
+                         modifier = Modifier
+                             .padding(innerPadding)
+                             .background(color = SecondaryColor), navController = navController
+                     )*/
                 }
             }
         }
@@ -83,11 +98,131 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ProfileScreen(){
+fun ProfileScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(SecondaryColor)
+    ) {
+        ProfileButton(
+            R.drawable.exit_icon,
+            exitButtonIconTint,
+            exitButtonBackground,
+            {}, Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 32.dp, end = 24.dp)
+        )
+        Column(
+            modifier = Modifier.align(Alignment.TopCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.profile_icon),
+                null,
+                modifier = Modifier
+                    .padding(top = 40.dp)
+                    .size(90.dp)
+            )
+            Text(
+                modifier = Modifier.padding(top = 5.dp),
+                textAlign = TextAlign.Center,
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = PrimaryColor,
+                            fontFamily = Nunito,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Medium
+                        )
+                    ) {
+                        append("Добрый вечер,\n")
+                    }
+                    withStyle(
+                        style = SpanStyle(
+                            color = PrimaryColor,
+                            fontFamily = Nunito,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Black
+                        )
+                    ) {
+                        append("Пользователь")
+                    }
+                }
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            ) {
+                val fieldList = listOf(
+                    Pair("Имя", "Игорь"),
+                    Pair("Фамилия", "Мяков"),
+                    Pair("Отчество", "Николаевич"),
+                    Pair("Email", "sepryn4ik@mail.ru"),
+                    Pair("Статус", "Студент")
+                )
 
+                fieldList.forEach { pair ->
+                    ProfileField(pair.first, pair.second)
+                }
+            }
+            ProfileButton(
+                R.drawable.edit_password_icon,
+                PrimaryButton,
+                editButtonBackground,
+                {}, Modifier
+            )
+        }
+    }
 }
 
+@Composable
+fun ProfileButton(
+    icon: Int,
+    iconTint: Color,
+    backgroundTint: Color,
+    onClick: () -> Unit,
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier
+            .size(48.dp)
+            .clip(CircleShape)
+            .background(backgroundTint)
+            .padding(8.dp)
+    ) {
+        IconButton(onClick = onClick) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+                tint = iconTint
+            )
+        }
+    }
+}
 
+@Composable
+fun ProfileField(fieldName: String, value: String) {
+    Column(modifier = Modifier.padding(top = 15.dp)) {
+        Text(
+            text = fieldName,
+            modifier = Modifier,
+            fontFamily = Nunito,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Gray
+        )
+        Text(
+            text = value,
+            modifier = Modifier,
+            fontFamily = Nunito,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            color = PrimaryColor
+        )
+        HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+    }
+}
 
 
 
