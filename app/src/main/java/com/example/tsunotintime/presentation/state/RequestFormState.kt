ff780b0b24174cd03216f5dac986fdfe91aba944
MIGHits <164134306+MIGHits@users.noreply.github.com>
2025-03-21 12:05:22 +0700

    fix error with displaying local date time

--- app/src/main/java/com/example/tsunotintime/presentation/state/RequestFormState.kt
+++ app/src/main/java/com/example/tsunotintime/presentation/state/RequestFormState.kt
@@ -5,13 +5,14 @@ import com.example.tsunotintime.AppContext.Companion.instance
 import com.example.tsunotintime.R
 import com.example.tsunotintime.common.Constant.EMPTY_RESULT
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 data class RequestFormState(
     val absenceDateFrom: String = DateTimeFormatter.ofPattern(instance.getString(R.string.isoSecondAccuracyFormat))
-        .format(LocalDateTime.now()),
+        .format(LocalDateTime.now(ZoneOffset.UTC)),
     val absenceDateTo: String = DateTimeFormatter.ofPattern(instance.getString(R.string.isoSecondAccuracyFormat))
-        .format(LocalDateTime.now()),
+        .format(LocalDateTime.now(ZoneOffset.UTC)),
     val description: String = EMPTY_RESULT,
     val images: List<Uri> = emptyList(),
     val isValid: Boolean = false
